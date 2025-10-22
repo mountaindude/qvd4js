ff4731c4362dfa777c360e1349e392afedcafa95
github-action-benchmark <github@users.noreply.github.com>
2025-10-22 15:45:38 +0000

    add qvd4js Benchmark - Linux - Node 24.x (customBiggerIsBetter) benchmark result for b7b2336a2282bfcc734f46fe3a0eed7e4e9ba310

--- benchmarks/Linux-node24.x/data.js
+++ benchmarks/Linux-node24.x/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761144469312,
+  "lastUpdate": 1761147938149,
   "repoUrl": "https://github.com/ptarmiganlabs/qvd4js",
   "entries": {
     "qvd4js Benchmark - Linux - Node 24.x": [
@@ -352,6 +352,65 @@ window.BENCHMARK_DATA = {
             "name": "Parse with maxRows=1000 (lazy loading)",
             "value": 50.160149797388925,
             "range": "±0.48%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "goran@ptarmiganlabs.com",
+            "name": "Göran Sander",
+            "username": "mountaindude"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7b2336a2282bfcc734f46fe3a0eed7e4e9ba310",
+          "message": "Merge pull request #52 from ptarmiganlabs/bugfix",
+          "timestamp": "2025-10-22T17:32:22+02:00",
+          "tree_id": "f3cada4b069512f59fb027f1a2e85a82911102ee",
+          "url": "https://github.com/ptarmiganlabs/qvd4js/commit/b7b2336a2282bfcc734f46fe3a0eed7e4e9ba310"
+        },
+        "date": 1761147938120,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Parse small QVD (~600 rows)",
+            "value": 230.37610465607028,
+            "range": "±0.96%",
+            "unit": "ops/sec",
+            "extra": "230 samples"
+          },
+          {
+            "name": "Parse medium QVD (~18k rows)",
+            "value": 5.962937492456762,
+            "range": "±1.21%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
+          },
+          {
+            "name": "Parse large QVD (~60k rows)",
+            "value": 2.3311652916853594,
+            "range": "±0.51%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
+          },
+          {
+            "name": "Parse with maxRows=100 (lazy loading)",
+            "value": 55.05286471040139,
+            "range": "±5.73%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
+          },
+          {
+            "name": "Parse with maxRows=1000 (lazy loading)",
+            "value": 49.083990700971725,
+            "range": "±0.60%",
             "unit": "ops/sec",
             "extra": "64 samples"
           }
